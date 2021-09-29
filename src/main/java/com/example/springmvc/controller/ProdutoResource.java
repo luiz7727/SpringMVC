de319a1dbf019f508422ea4dae3703e34602f6ee
luiz7727 <luiz.antonio999@hotmail.com>
2021-09-29 19:30:34 -0300

    Anotando as annotations e configurando a classe ProdutoResource

--- src/main/java/com/example/springmvc/controller/ProdutoResource.java
+++ src/main/java/com/example/springmvc/controller/ProdutoResource.java
@@ -1,4 +1,10 @@
 package com.example.springmvc.controller;
 
+import com.example.springmvc.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProdutoResource {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
 }
--- src/main/java/com/example/springmvc/repository/ProdutoRepository.java
+++ src/main/java/com/example/springmvc/repository/ProdutoRepository.java
@@ -1,4 +1,18 @@
 package com.example.springmvc.repository;
 
-public interface ProdutoRepository {
+import com.example.springmvc.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+
+    //Assinaturas referentes ao banco de dados
+    //Select:findAll() ou findById()
+    //INSERT ou UPDATE: save()
+    //DELETE: deleteById()
+    List<Produto> findByNome(String prod);
+    List<Produto> findByNovo(Boolean prod);
+    List<Produto> findByNomeAndNovo(String prod,boolean novo);
+    List<Produto> findByPrecoGreaterThan(double preco);
 }
